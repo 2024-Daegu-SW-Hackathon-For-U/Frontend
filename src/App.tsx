39d3554a8ca43b7b7221f1ac30c8c6957bf7e9b4
seung365 <bdh3659@naver.com>
2024-09-21 15:41:59 +0900

    chore: UI 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css';
+import HomePage from './pages/home';
 
 function App() {
-	return <></>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,15 @@
+import { SearchButtonL, SearchButtonM } from '@/components/Home/Buttons';
+import Search from '@/components/Home/Search';
+
 const HomePage = () => {
-	return (
-		<>
-			<h1>Home Page</h1>
-		</>
-	);
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Search />
+      <SearchButtonM label='내 주변 장소 찾기' />
+      <SearchButtonL label='내 주변 장소 찾기' />
+    </>
+  );
 };
 
 export default HomePage;
