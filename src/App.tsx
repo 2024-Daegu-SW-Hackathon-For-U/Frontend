b8c3c8d9a8d4a4db8db9e2744573608809532725
Dobbymin <gmin0701@knu.ac.kr>
2024-09-21 13:40:52 +0900

    Feat(setting): global style 세팅 및 provider 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import './App.css';
+import { queryClient } from './api/instance';
+import { Routes } from './routes';
+import GlobalTheme from './styles/global';
+import { ChakraProvider, GlobalStyle } from '@chakra-ui/react';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
-	return <></>;
+  return (
+    <ChakraProvider theme={GlobalTheme}>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <Routes />
+      </QueryClientProvider>
+    </ChakraProvider>
+  );
 }
 
 export default App;
