b07de476c49f6ea5bdfaee60fb8ecde77e64621a
beom <74394824+seung365@users.noreply.github.com>
2024-09-21 23:02:32 +0900

    추천 테마 지도 UI 구현 (#36)
    * Chore: Search 컴포넌트 파일 import 추가
    
    * Chore: gitignore 추가
    
    * chore: kakao map dclare 설정
    
    * Style: pnpm-lock.yaml 줄바꿈
    
    * Chore: 환경 변수 설정
    
    * Design(map): map UI에 들어가는 이미지 추가
    
    * Build: index.html에 kakao map api 연결
    
    * Feat(route): recommend-map 추가
    
    * Feat(recommendMap): recommend-map에 들어갈 컴포넌트 추가
    
    * Feat(recommendMap): recommendMap UI 구현

---
+++ src/pages/recommendMap/index.tsx
@@ -0,0 +1,57 @@
+import { useEffect } from 'react';
+
+import { BackButtonWhite } from '@/components/RecommendMap/BackButton';
+import { PlusButtton } from '@/components/RecommendMap/PlusButton';
+import styled from '@emotion/styled';
+
+const RecommandMap = () => {
+  useEffect(() => {
+    const container = document.getElementById('map');
+    if (container) {
+      const options = {
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+      const map = new window.kakao.maps.Map(container, options);
+    } else {
+      console.error('Map container not found');
+    }
+  }, []);
+
+  return (
+    <Wrapper id='map' style={{ width: '100%', height: '100vh' }}>
+      <BackButtonsWrapper>
+        <BackButtonWhite label='공부하기 좋은 카페' />
+      </BackButtonsWrapper>
+      <PlusButtonsWrapper>
+        <PlusButtton label='장소 추천하기' />
+      </PlusButtonsWrapper>
+    </Wrapper>
+  );
+};
+
+export default RecommandMap;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+`;
+
+const BackButtonsWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  top: 6%;
+  z-index: 100;
+  position: absolute;
+`;
+
+const PlusButtonsWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  top: 90%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 100;
+  position: absolute;
+`;
