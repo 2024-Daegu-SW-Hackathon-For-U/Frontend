1f30d219ca6a2d44363df877cdac65520860f182
Dobbymin <gmin0701@knu.ac.kr>
2024-09-21 19:16:09 +0900

    Feat(router): 마이페이지 이동 router 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,29 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 
 import { RouterPath } from './path';
+import Header from '@/components/features/layout/header';
 import HomePage from '@/pages/home';
+import MyPage from '@/pages/mypage';
 
 const router = createBrowserRouter([
   {
     path: RouterPath.root,
-    element: <HomePage />,
+    element: (
+      <>
+        <Header />
+        <Outlet />
+      </>
+    ),
+    children: [
+      {
+        path: RouterPath.root,
+        element: <HomePage />,
+      },
+      {
+        path: RouterPath.mypage,
+        element: <MyPage />,
+      },
+    ],
   },
 ]);
 
