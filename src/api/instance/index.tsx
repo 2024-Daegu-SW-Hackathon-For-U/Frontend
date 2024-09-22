a79c9bba5e2689a8dc2079ad6228fb4c4e919bb7
Dobbymin <gmin0701@knu.ac.kr>
2024-09-22 15:34:48 +0900

    Feat(api): accessToken 추가 코드 구현

--- src/api/instance/index.tsx
+++ src/api/instance/index.tsx
@@ -1,4 +1,8 @@
-import type { AxiosInstance, AxiosRequestConfig } from 'axios';
+import type {
+  AxiosInstance,
+  AxiosRequestConfig,
+  InternalAxiosRequestConfig,
+} from 'axios';
 import axios from 'axios';
 
 import { BASE_URL } from '@/constants/URI';
@@ -32,3 +36,17 @@ export const queryClient = new QueryClient({
     },
   },
 });
+
+fetchInstance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken !== undefined) {
+      config.headers['Content-Type'] = 'application/json';
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
