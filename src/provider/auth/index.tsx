9c070d16bdc8f5bddd84b221ecf6c22372973cb9
Dobbymin <gmin0701@knu.ac.kr>
2024-09-22 17:05:14 +0900

    Feat(auth): auth provider 구현

---
+++ src/provider/auth/index.tsx
@@ -0,0 +1,49 @@
+import type { ReactNode } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+
+import { authLocalStorage } from '@/utils/storage';
+
+type AuthInfo = {
+  token: string;
+};
+
+type AuthContextData = {
+  authInfo: AuthInfo | undefined;
+  setAuthInfo: (authInfo: AuthInfo) => void;
+};
+
+const AuthContext = createContext<AuthContextData | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const currentAuthToken = authLocalStorage.get();
+  const [isReady, setIsReady] = useState(false);
+  const [authInfo, setAuthInfo] = useState<AuthInfo | undefined>(undefined);
+
+  const handleAuthInfo = (currentAuthInfo: AuthInfo) => {
+    setAuthInfo(currentAuthInfo);
+    authLocalStorage.set(currentAuthInfo.token);
+  };
+
+  useEffect(() => {
+    if (currentAuthToken) {
+      setAuthInfo({ token: currentAuthToken });
+    }
+    setIsReady(true);
+  }, [currentAuthToken]);
+
+  if (!isReady) return null;
+
+  return (
+    <AuthContext.Provider value={{ authInfo, setAuthInfo: handleAuthInfo }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = (): AuthContextData => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
