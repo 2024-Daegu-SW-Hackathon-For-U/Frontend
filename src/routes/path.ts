3854eb76a6462b0da7161397eb7651f97101322a
seung365 <bdh3659@naver.com>
2024-09-22 17:55:38 +0900

    fix: 버그 수정

--- src/routes/path.ts
+++ src/routes/path.ts
@@ -1,11 +1,12 @@
-export const RouterPath = {
+const routes = {
   root: '/',
-  login: 'login',
-  mypage: 'mypage',
-  recommendMap: (id: string) => `recommend-map/${id}`,
-  curationMap: (id: string) => `curation-map/${id}`,
-  myMapSetting: 'mymap-setting',
+  login: '/login',
+  mypage: '/mypage',
+  recommendMap: (id: string) => `/recommend-map/${id}`,
+  curationMap: (id: string) => `/curation-map/${id}`,
+  myMapSetting: '/my-map-setting',
   recommendAdd: (id: string) => `/recommend-map/${id}/add`,
   curationAdd: (id: string) => `/curation-map/${id}/add`,
-  searchPlace: (keyword: string) => `/keyword/${keyword}`,
+  searchPlace: (keyword: string) => `/search-place?keyword=${keyword}`,
+  kakaoCallback: '/kakao/callback', // 여기에 추가
 };
